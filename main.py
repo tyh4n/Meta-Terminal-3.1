0fde7c8f46ec3ee78d940c94964087dd897ed8ad
tyh4n <tianyih4@illinois.edu>
2024-07-19 19:12:02 -0500

    initial commit

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-from pyhocon import ConfigFactory
-
 from device import Manager_Base, Serial_Manager, Socket_Manager, Bluetooth_Manager
-from graph import Coordinatograph
 from chart_widget import ChartList
 from command_widget import CommandPanel
 
@@ -18,31 +15,37 @@ class Meta_UI(QWidget):
 
     def __init__(self):
         super(Meta_UI, self).__init__()
-        self.received_data = ''
-        self.communicate_manager = Manager_Base('')
+        self.received_data = ''  # Initialize received data variable
+        self.communicate_manager = Manager_Base('')  # Initialize communication manager
 
+        # Create main widgets
         self.terminal_widget = QWidget(self)
-        self.param_adjust_tab = QWidget(self)
+        self.plotting_tab = QWidget(self)
 
+        # Setup main splitter to divide terminal and plotting tab
         main_splitter = QSplitter(Qt.Horizontal)
         main_splitter.addWidget(self.terminal_widget)
-        main_splitter.addWidget(self.param_adjust_tab)
+        main_splitter.addWidget(self.plotting_tab)
+
+        # Set initial sizes for the widgets in the splitter
+        main_splitter.setSizes([800, 800])  # Adjust the values to make the plotting tab wider
+
+        # Setup main layout
         main_layout = QHBoxLayout()
-        # main_layout.setContentsMargins(0, 0, 0, 0)
         main_layout.addWidget(main_splitter)
         self.setLayout(main_layout)
 
+        # Setup widgets
         self.setup_terminal_widget()
-        self.setup_param_adjust_tab()
+        self.setup_plotting_tab()
 
-        # self.resize(500, 500)
-
-        self.setWindowTitle('Meta Terminal III')
+        # Set window properties
+        self.setWindowTitle('Meta Terminal 3.1')
         self.setWindowIcon(QtGui.QIcon('res/meta_logo.jpeg'))
+        self.resize(1600, 1200)  # Set the initial window size
 
     def setup_terminal_widget(self):
-
-        # Elements setup
+        # Elements setup for terminal widget
         connection_port_combo = QComboBox()
         connection_port_list = ['Serial', 'TCP']
         connection_port_combo.addItems(connection_port_list)
@@ -64,35 +67,7 @@ class Meta_UI(QWidget):
         clear_data_button = QPushButton()
         clear_data_button.setText('Clear')
 
-        self.project_title = 'Meta'
-        self.project_label = QLabel(self.project_title)
-        title_font = QFont('Times New Roman', 30, QFont.Bold)
-        self.project_label.setFont(title_font)
-
-        title_spacer = QSpacerItem(100, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.load_config_file_button = QPushButton()
-        # self.load_config_file_button.setSizePolicy(QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum))
-        self.load_config_file_button.setText('Load Config')
-        self.load_config_file_button.clicked.connect(self.open_file)
-
-        self.export_button = QPushButton()
-        # self.export_button.setSizePolicy(QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum))
-        self.export_button.setText('Export Params')
-        self.export_button.clicked.connect(self.save_file)
-
         # Layout setup
-        file_buttons_layout = QVBoxLayout()
-        file_buttons_layout.setContentsMargins(0, 0, 0, 0)
-        file_buttons_layout.addWidget(self.load_config_file_button)
-        file_buttons_layout.addWidget(self.export_button)
-
-        header_layout = QHBoxLayout()
-        header_layout.setContentsMargins(0, 0, 0, 0)
-        header_layout.addWidget(self.project_label, alignment=Qt.AlignTop)
-        header_layout.addItem(title_spacer)
-        header_layout.addLayout(file_buttons_layout)
-
         control_button_layout = QHBoxLayout()
         control_button_layout.setContentsMargins(0, 0, 0, 0)
         control_button_layout.addWidget(connection_port_combo)
@@ -106,29 +81,31 @@ class Meta_UI(QWidget):
         command_line_layout.addWidget(clear_data_button)
 
         terminal_part_layout = QVBoxLayout()
-        terminal_part_layout.addLayout(header_layout)
         terminal_part_layout.addLayout(control_button_layout)
         terminal_part_layout.addWidget(self.terminal_display)
         terminal_part_layout.addLayout(command_line_layout)
 
         # Event Callback Setup
-
         def update_connect_button(set_on: bool):
+            # Update the connection button text based on the connection status
             if set_on:
                 connection_button.setText('Disconnect')
             else:
                 connection_button.setText('Connect')
 
         def clear_data():
+            # Clear the terminal display
             self.terminal_display.clear()
 
         # Connections setup
         def command_line_send_msg():
+            # Send the message from the command line
             msg = command_line.text()
             self.send_msg(msg)
             command_line.clear()
 
         def connection_button_clicked():
+            # Handle the connection button click event
             if connection_button.text() == 'Connect':
                 method = connection_port_combo.currentText()
                 device = port_device_text.text()
@@ -151,206 +128,28 @@ class Meta_UI(QWidget):
         clear_data_button.clicked.connect(clear_data)
         self.terminal_widget.setLayout(terminal_part_layout)
 
-    def setup_param_adjust_tab(self):
-
+    def setup_plotting_tab(self):
         main_splitter = QSplitter(Qt.Vertical)
 
-        # ***************************************** Global Function Section ****************************************** #
-        self.global_func_panel = CommandPanel(parent=self,
-                                              command_list=['temp_global temp_global_param', 'temp_button'],
-                                              send_callback=self.send_msg)
-
-        global_func_layout = QVBoxLayout()
-        global_func_layout.addWidget(self.global_func_panel)
-        global_func_container = QWidget()
-        global_func_container.setLayout(global_func_layout)
-
-        main_splitter.addWidget(global_func_container)
-
-        # ******************************************* Chart Display Section ****************************************** #
-        self.chart_list = ChartList(self.param_adjust_tab, [{'name': 'temp'}])
-
-        chart_height_label = QLabel("Chart Height")
-        self.chart_height_spin = QSpinBox()
-        self.chart_height_spin.setRange(0, 9999)
-        self.chart_height_spin.setSingleStep(50)
-        self.chart_height_spin.setValue(250)
-        self.chart_height_spin.valueChanged.connect(self.chart_list.set_chart_height)
-
-        chart_control_spacer = QSpacerItem(100, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.angle_visibility_check = QCheckBox()
-        self.angle_visibility_check.setText("Angle")
-        self.angle_visibility_check.setChecked(True)
-        self.angle_visibility_check.stateChanged.connect(self.set_chart_visibilities)
-        self.velocity_visibility_check = QCheckBox()
-        self.velocity_visibility_check.setText("Velocity")
-        self.velocity_visibility_check.setChecked(True)
-        self.velocity_visibility_check.stateChanged.connect(self.set_chart_visibilities)
-        self.current_visibility_check = QCheckBox()
-        self.current_visibility_check.setText("Current")
-        self.current_visibility_check.setChecked(True)
-        self.current_visibility_check.stateChanged.connect(self.set_chart_visibilities)
+        # Chart Display Section
+        self.chart_list = ChartList(self.plotting_tab, [{'name': 'temp'}])
 
         chart_layout = QGridLayout()
-        chart_layout.addWidget(chart_height_label, 0, 0, 1, 1)
-        chart_layout.addWidget(self.chart_height_spin, 0, 1, 1, 1)
-        chart_layout.addItem(chart_control_spacer, 0, 2, 1, 1)
-        chart_layout.addWidget(self.angle_visibility_check, 0, 3, 1, 1)
-        chart_layout.addWidget(self.velocity_visibility_check, 0, 4, 1, 1)
-        chart_layout.addWidget(self.current_visibility_check, 0, 5, 1, 1)
+
         chart_layout.addWidget(self.chart_list, 1, 0, 1, 6)
         chart_container = QWidget()
         chart_container.setLayout(chart_layout)
 
         main_splitter.addWidget(chart_container)
 
-        # ***************************************** Motor Function Section ******************************************* #
-        self.motor_combo = QComboBox()
-        self.clear_param_button = QPushButton()
-        self.clear_param_button.setText('ClearParams')
-        self.clear_param_button.clicked.connect(self.clear_params)
-        self.save_button = QPushButton()
-        self.save_button.setText('Save')
-        self.save_button.clicked.connect(self.save_params)
-        self.reload_button = QPushButton()
-        self.reload_button.setText('Reload')
-        self.reload_button.clicked.connect(self.reload_params)
-        self.delete_button = QPushButton()
-        self.delete_button.setText('Delete')
-        self.delete_button.clicked.connect(self.delete_history)
+        plotting_layout = QVBoxLayout()
+        plotting_layout.setContentsMargins(0, 0, 0, 0)  # each section has its own margin
+        plotting_layout.addWidget(main_splitter)
 
-        tool_layout = QGridLayout()
-
-        tool_layout.addWidget(self.motor_combo, 0, 0, 1, 1)
-        tool_layout.addWidget(self.clear_param_button, 0, 1, 1, 1)
-        tool_layout.addWidget(self.save_button, 0, 2, 1, 1)
-        tool_layout.addWidget(self.reload_button, 0, 3, 1, 1)
-        tool_layout.addWidget(self.delete_button, 0, 4, 1, 1)
-
-        self.motor_stacked_layout = QStackedLayout()
-        self.setup_motor_panel(motor_config=[])
-
-        tool_layout.addLayout(self.motor_stacked_layout, 1, 0, 1, 5)
-        tool_widget = QWidget()
-        tool_widget.setLayout(tool_layout)
-        main_splitter.addWidget(tool_widget)
-
-        main_splitter.setSizes([150, 400, 300])
-
-        param_adjust_layout = QVBoxLayout()
-        param_adjust_layout.setContentsMargins(0, 0, 0, 0)  # each section has its own margin
-        param_adjust_layout.addWidget(main_splitter)
-
-        self.param_adjust_tab.setLayout(param_adjust_layout)
-
-    def setup_motor_panel(self, motor_config):
-        for widget in self.motor_stacked_layout.children():
-            self.motor_stacked_layout.removeWidget(widget)
-        self.motor_combo.clear()
-        self.motor_combo.addItems((motor['name'] for motor in motor_config))
-        for motor in motor_config:
-            # widget = QWidget()
-            # widget_layout = QHBoxLayout(widget)
-            splitter = QSplitter(Qt.Horizontal)
-            command_panel = CommandPanel(splitter, command_list=motor['commands'], predefined=motor['predefined'],
-                                         send_callback=self.send_msg)
-            command_panel.setObjectName('command_panel')
-            param_history = QListWidget(splitter)
-            param_history.setDragDropMode(QAbstractItemView.DragDrop)
-            param_history.setDefaultDropAction(Qt.MoveAction)
-            param_history.setSelectionMode(QAbstractItemView.ExtendedSelection)
-            # param_history.doubleClicked.connect(print)
-            param_history.setObjectName('param_history')
-            # splitter.addWidget(command_panel)
-            # splitter.addWidget(param_history)
-            # widget_layout.addWidget(command_panel)
-            # widget_layout.addWidget(param_history)
-            # self.motor_stacked_layout.addWidget(widget)
-            self.motor_stacked_layout.addWidget(splitter)
-        self.motor_combo.currentIndexChanged.connect(self.motor_stacked_layout.setCurrentIndex)
-
-    def set_chart_visibilities(self):
-        self.chart_list.set_chart_visibilities(
-            self.angle_visibility_check.isChecked(),
-            self.velocity_visibility_check.isChecked(),
-            self.current_visibility_check.isChecked()
-        )
-
-    def open_file(self):
-        fileName, fileType = QtWidgets.QFileDialog.getOpenFileName(None, "choose file", os.getcwd(), "All Files(*)")
-        if not fileName:
-            return
-        self.readConfigFromFileName(fileName)
-    def readConfigFromFileName(self,fileName):
-        # Read the config file
-        config_dict = ConfigFactory.parse_file(fileName)
-        self.project_title = getattr(config_dict, 'project', 'temp')
-        motor_config = getattr(config_dict, 'motor_config')
-        if motor_config is None:
-            return
-        motors = []
-        for i, m_config in enumerate(motor_config):
-            motor = {}
-            motor['id'] = getattr(m_config, 'motor_id', i)
-            motor['name'] = getattr(m_config, 'motor_name', 'motor_%d' % (i))
-            motor['predefined'] = {'motor_id': motor['id']}
-            motor['commands'] = []
-            motors.append(motor)
-        self.id2idx = {motor['id']: i for i, motor in enumerate(motors)}
-        self.name2idx = {motor['name']: i for i, motor in enumerate(motors)}
-
-        commands = list(getattr(config_dict, 'commands', []))
-        global_funcs = []
-        for command in commands:
-            if '->' in command:
-                command, motor_names = command.split('->', 1)
-                motor_names = motor_names.split()
-                for name in motor_names:
-                    if name in self.name2idx:
-                        motors[self.name2idx[name]]['commands'].append(command)
-            elif 'motor_id' in command:
-                for motor in motors:
-                    motor['commands'].append(command)
-            else:
-                global_funcs.append(command)
-        self.global_func_panel.setup_list_rows(global_funcs, send_callback=self.send_msg)
-
-        # Read config done. Build structure
-
-        self.project_label.setText(self.project_title)
-        self.chart_list.setup_list_rows(motors)
-        self.setup_motor_panel(motors)
-
-    def save_params(self):
-        current_motor_splitter = self.motor_stacked_layout.currentWidget()
-        command_panel = current_motor_splitter.findChild(CommandPanel)
-        param_history = current_motor_splitter.findChild(QListWidget)
-        commands = command_panel.get_elements()
-        new_historys = ['%s %s' % (command_name, ' '.join(params)) for command_name, params in commands]
-        param_history.addItems(new_historys)
-
-    def reload_params(self):
-        current_motor = self.motor_stacked_layout.currentWidget()
-        command_panel = current_motor.findChild(CommandPanel)
-        param_history = current_motor.findChild(QListWidget)
-        items = param_history.selectedItems()
-        elements = [item.text() for item in items]
-        command_panel.set_elements(elements)
-
-    def clear_params(self):
-        current_motor = self.motor_stacked_layout.currentWidget()
-        command_panel = current_motor.findChild(CommandPanel)
-        command_panel.clear_elements()
-
-    def delete_history(self):
-        current_motor = self.motor_stacked_layout.currentWidget()
-        param_history = current_motor.findChild(QListWidget)
-        for item in param_history.selectedItems():
-            row = param_history.row(item)
-            param_history.takeItem(row)
+        self.plotting_tab.setLayout(plotting_layout)
 
     def process_feedback(self, feedback: bytes):
+        # Process received feedback from the device
         self.received_data += feedback.decode(encoding='utf-8')
         if '\n' in self.received_data:
             fine_data, self.received_data = self.received_data.rsplit('\n', 1)
@@ -365,12 +164,14 @@ class Meta_UI(QWidget):
                     self.update_terminal_display(line + '\n')
 
     def update_terminal_display(self, input_str: str):
+        # Update the terminal display with new input
         cursor = self.terminal_display.textCursor()
         cursor.movePosition(QtGui.QTextCursor.End)
         cursor.insertText(input_str)
         self.terminal_display.setTextCursor(cursor)
 
     def send_msg(self, msg):
+        # Send a message to the device
         try:
             self.communicate_manager.SendData(bytes(msg + '\r\n', encoding='utf-8'))
         except Exception as err:
@@ -378,41 +179,8 @@ class Meta_UI(QWidget):
             msg = 'Fail to send message!'
         self.update_terminal_display(msg + '\n')
 
-    def save_file(self):
-        fileName, fileType = QtWidgets.QFileDialog.getSaveFileName(None, "choose file", os.path.join(os.getcwd(),
-                                                                                                     self.project_title + '_param.txt'),
-                                                                   "All Files(*)")
-        if not fileName:
-            return
-
-        with open(fileName, 'w', encoding='utf-8') as file_save:
-            content = []
-            content.append(self.project_title)
-            content.append('')
-
-            content.append('Global Function Parameters')
-            global_func_count = self.global_func_panel.vlistWidget.count()
-            for i in range(global_func_count):
-                item = self.global_func_panel.vlistWidget.item(i)
-                content.append('%s %s' % (item.widget.findChild(QLabel).text(), ' '.join(item.get_params())))
-            content.append('')
-
-            motor_count = self.motor_combo.count()
-            for i in range(motor_count):
-                content.append(self.motor_combo.itemText(i) + ' Function Parameters')
-                current_motor = self.motor_stacked_layout.widget(i)
-                command_panel = current_motor.findChild(CommandPanel)
-                motor_func_count = command_panel.vlistWidget.count()
-                for j in range(motor_func_count):
-                    item = command_panel.vlistWidget.item(j)
-                    content.append('%s %s' % (item.widget.findChild(QLabel).text(), ' '.join(item.get_params())))
-                content.append('')
-
-            file_save.write('\n'.join(content))
-
-
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    demo = Meta_UI()
-    demo.show()
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv)  # Create a QApplication instance
+    demo = Meta_UI()  # Instantiate the main window
+    demo.show()  # Show the main window
+    sys.exit(app.exec_())  # Execute the application's event loop
--- readme.md
+++ readme.md
@@ -1,80 +1,67 @@
-# Meta-Terminal III
 
-> This is the source code for the Meta-Terminal III. The Meta-Terminal III is a tool that helps the Meta Team adjust parameters with configurable interface. This tool is written in python with the modules from PyQT5.
+# Meta Terminal 3.1
 
-You can run the tool from the source code by typing
+Forked from [Meta Terminal III](https://github.com/Meta-Team/Meta-Terminal-III), Meta Terminal 3.1 is a graphical user interface (GUI) application designed for interacting with various devices via serial, TCP, and Bluetooth connections. The application provides functionalities to send commands, display terminal output, and plot data received from the devices.
 
-```shell
-python3 main.py
-```
+## Features
 
-in your computer terminal under the present directory.
+- Connect to devices via Serial, TCP, or Bluetooth.
+- Send commands to connected devices.
+- Display terminal output in real-time.
+- Plot data received from devices in real-time.
 
-## Install Requests
+## Requirements
 
-```bash
-pip3 install -r requests.txt
-```
+- Python 3.6+
+- PyQt5
+- pyhocon
+- matplotlib
 
+## Installation
 
-## Use of configuration file
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/h4nty/meta-terminal-3.1.git
+   cd meta-terminal-3.1
+   ```
 
-The configuration file helps you define your testing interface. There are some presevered keywords and you can define your own parameters and commands so long as you follow the rules. The configuration file must be consistant with the test code and you should double check before running testing.
+2. Install the required Python packages:
+   ```bash
+   pip install -r requirements.txt
+   ```
 
-### Preserved keywords
+## Usage
 
-+ **project**: The name of the test, not important.
-+ **motor_config**: A list of configurations for each motor.
-+ **motor_id**: The id of the motor. The number will be fit into the command indicating the motor to which the command is sent. id should be unique and consistant with your code.
-+ **motor_name**: The name for the motor, marking the motor in the interface.
-+ **commands**: Put any other commands that you will need in your testing here.
-    + If the command may be used for each motor seperately, set the first parameter **"motor_id"**. 
-        + For example, command "fb_enable motor_id set_enable" will be placed under each motor with only one input parameter *set_enable*.
-    + If the command is only for one or more but not all motors, list the motor names in the command and put a **"->"** before the names start.
-        + For example, command "a2v_enable motor_id set_enable -> yaw pitch bullet" will only be placed under *yaw*, *pitch* and *bullet*. 
-    + If the command has neither **"motor_id"** in the first place nor a **"->"** in the middle, the command will be considered as a global command.
-        + For example, "remote_enable set_enable" will be considered as a global command.
+1. Run the application:
+   ```bash
+   python main.py
+   ```
 
-## Format of feedback
+2. Use the GUI to connect to a device:
+   - Select the connection method (Serial or TCP) from the dropdown.
+   - Enter the port or device address.
+   - Click "Connect".
 
-For the convenience of graphic display, the feedback should follow some patterns:
+3. Send commands to the connected device:
+   - Enter the command in the text box at the bottom.
+   - Click "Send".
 
-+ For the **motor feedback** message, the feedback should follow the pattern of **"fb \[motor_name] \[angle] \[target_angle] \[velocity] \[target_velocity] \[current] \[target_current]"** (replace the value in "[]" with real value). Make sure that each feedback message has **6 numerical parameters**, fill up the parameters with 0 if you have no value for it. The **\[motor_name]** should be consistant with the **motor_name** in your configuration file.
-    + For example, the feedback could be "fb yaw 356.7 360.0 3.7 4.5 0 0".
-+ For the **pure textual** message, please avoid starting with "fb".
+4. Clear the terminal display by clicking the "Clear" button.
 
-> An example of configuration file *template.json* and test file *pa_example.cpp* can be seen in the *example* folder.
+5. View the plotted data on the right side of the window.
 
-## Interface Introduction
+## File Overview
 
-![Basic Interface](./res/basic_interface.png)
+- `main.py`: The main entry point of the application. Sets up the GUI and handles user interactions.
+- `device.py`: Manages device connections and communication for Serial, TCP, and Bluetooth methods.
+- `chart_widget.py`: Manages the creation and updating of charts for plotting data received from devices.
 
-1. After typing ```python3 main.py``` in the terminal, you can see the above interface. The general interface consists of two parts:
-    + **Parameter Adjust Interface**: The main interface where you set commands and visualize the motor feedback.
-    + **Terminal Interface**: The interface where you setup connection with the device and send command manually.
-    + Click the "Load config" button to load the configuration file.
-    + Click "Export" button (Under the "Load config" button, not shown in the image) to export all the data.
+## Removed Features
 
-![Terminal Area](./res/terminal_area.png)
+- The parameter modifier functionality has been removed.
+- The configuration save and load features have been removed.
 
-2. In the **Terminal Interface**, choose a device as you communication tool, fill the port name(for serial) or device ip(for tcp) and click **connect** to setup the connection. The **connect** button will become **disconnect** if connection is completed. The big white block in the middle displays the terminal message. You can type command in the input block at the bottom and send by clicking **send** button.
+## Contributing
 
-![Chart Display](./res/chart_area.png)
+Contributions are welcome! Please fork the repository and submit pull requests for any improvements or bug fixes.
 
-3. The **Chart Display** displays the motor feedback. In the each chart, the red line represent the target value and the blue line represent the actual value.
-
-![Function Area](./res/function_area.png)
-![param_2](./res/param_2.png)
-![param_1](./res/param_1.png)
-
-4. There are two function blocks: Global Function and Motor Function. Global Function contains commands that are not motor-specific, and Motor Function contains commands that are motor-specific.
-    + Commands with no parameter are shown as buttons; commands with parameters are shown as lines of inputs and a send button.
-    + In the Motor Function block, choose the motor and operate each motor seperately.
-    + Select command lines and click **Clear** to clear parameters.
-    + Select command lines and click **Save** to save the parameters to history.
-    + Select history lines and click **Reload** to reload the history.
-    + Select history lines and click **Delete** to delete the history.
-    + The parameter could be either a *constant* or a *python expression* (see the second and third figure).
-        + If the **t(ms)** input is left blank, click the **send** button will send the command once (parameter must be a constant value, not python expression).
-        + If the **t(ms)** input is filled with a time duration in ms (1 sec = 1000 ms), click the **send** button will constantly send the command during the length of time with the interval of 20 ms. The value could be a constant or a python expression taking only **t** as the variable. The unit of t is also in ms.
-        + **Notice that the target value will not be set to 0 after the time duration, so be careful when setting the velocity and current**
